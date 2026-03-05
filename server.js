af72e1e5694e3fd73015c182f2e9da6510c116f9
Gideon Sleek <kapyengadeux27@gmail.com>
2026-03-05 10:41:53 +0300

    Update server.js

--- server.js
+++ server.js
@@ -146,3 +146,4 @@ app.post('/api/callback', (req, res) => {
 ========================= */
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+console.log("Safaricom response:", data);
